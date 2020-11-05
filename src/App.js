3ce4e2190804081a01752fc1c03b11a52392aa02
장재성 <jjs@jangjaeseong-ui-MacBookPro.local>
2020-11-05 14:10:49 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Row from './Row'
+import requests from './Requests'
+import './App.css'
+import Banner from './Banner'
+import Nav from './Nav'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className = "App">
+      <Nav />
+      <Banner />
+      <Row isLargeRow title = "Netflix Originals" fetchUrl = {requests.fetchNetflixoriginals}/>
+      <Row title = "Trending Now" fetchUrl = {requests.fetchTrending}/>
+      <Row title = "Top Rated" fetchUrl = {requests.fetchTopRated}/>
+      <Row title = "Comedy Movies" fetchUrl = {requests.fetchComedyMovies}/>
+      <Row title = "Horror Movies" fetchUrl = {requests.fetchHorrorMovies}/>
+      <Row title = "Romance Movies" fetchUrl = {requests.fetchRomanceMovies}/>
+      <Row title = "Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+// 015d4d61a781e9ae779f3a8e23a7615a
